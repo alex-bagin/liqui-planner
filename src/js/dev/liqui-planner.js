4f84f23901c600041ae922ee8a6bec18c6333075
Alex <alex.bagin@gmx.de>
2023-09-04 21:40:51 +0200

    done

---
+++ src/js/dev/liqui-planner.js
@@ -0,0 +1,15 @@
+/**
+ * Das Hauptmodul "liqui-planer" ist für die Instanziierung des Haushaltsbuchs und den
+ * Start der Anwendung zuständig.
+ * @module liqui-planner  
+ */
+
+import Haushaltsbuch from "./classes/Haushaltsbuch.js";
+
+/**
+ * Instanziierung des Haushaltsbuchs und Start der Anwendung.
+ */
+let liqui_planner = new Haushaltsbuch();
+liqui_planner.start();
+
+export default liqui_planner;
